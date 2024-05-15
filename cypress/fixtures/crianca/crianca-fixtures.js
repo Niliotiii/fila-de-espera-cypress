ecb2137904e3b70e2505e639ab3c10805405ec2b
Wallyson <machadodelimawallyson@gmail.com>
2024-05-15 15:45:23 -0400

    refactor: Update fixtures and commands for secretaria-educacao and unidade-escolar

--- cypress/fixtures/crianca/crianca-fixtures.js
+++ cypress/fixtures/crianca/crianca-fixtures.js
@@ -1,15 +1,34 @@
+import {
+  contatoFixtures as contato,
+  responsavelFixtures as responsavel,
+} from '../../fixtures';
+
 const faker = require('faker-br');
 
 export const criancaFixtures = {
-    nome: faker.name.findName(),
-    cpf: faker.br.cpf({ format: true }),
-    dataNascimento: new Date(
-        new Date().getFullYear() - Math.floor(Math.random() * 5) - 1,
-        Math.floor(Math.random() * 12), // Meses de 0 a 11
-        Math.floor(Math.random() * 31) + 1 // Dias de 1 a 31
-    ).toLocaleDateString('pt-BR', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit'
-    }),
+  // Identificação
+  nome: faker.name.findName(),
+  cpf: faker.br.cpf({ format: true }),
+  sexo: 'Masculino',
+  dataNascimento: new Date(
+    new Date().getFullYear() - Math.floor(Math.random() * 5) - 1,
+    Math.floor(Math.random() * 12),
+    Math.floor(Math.random() * 31) + 1,
+  ).toLocaleDateString('pt-BR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }),
+  numeroSUS: '123456789012345',
+  // Dados Residenciais
+  endereco: {
+    cep: '76909-836',
+    numero: '3800',
+    gerarLocalizacao: false,
+  },
+  //endereco: endereco(),
+
+  // Filiação
+  contato: contato,
+  responsavel: responsavel,
 };
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,20 +1,23 @@
-import './commands/auth/auth-login-commands.js'
-import './commands/endereco/endereco-commands.js'
-import './commands/contato/contato-commands.js'
-import './commands/agendamento/agendamento-commands.js'
-import './commands/agendamento/configurar-agendamento-commands.js'
-import './commands/secretaria-educacao/secretaria-educacao-commands'
-import './commands/context/context-secretaria-educacao-commands.js'
-import './commands/unidade-escolar/unidade-escolar-commands.js'
+import "./commands/agendamento/agendamento-commands.js";
+import "./commands/agendamento/configurar-agendamento-commands.js";
+import "./commands/auth/auth-login-commands.js";
+import "./commands/contato/contato-commands.js";
+import "./commands/context/context-secretaria-educacao-commands.js";
+import "./commands/crianca/crianca-commands.js";
+import "./commands/endereco/endereco-commands.js";
+import "./commands/secretaria-educacao/secretaria-educacao-commands";
+import "./commands/unidade-escolar/unidade-escolar-commands.js";
 
-Cypress.Commands.add('commands', (key, params) => {
-  const command = key.split('.').map((item) => item.charAt(0).toUpperCase() + item.slice(1)).join('')
+Cypress.Commands.add("commands", (key, params) => {
+  const command = key
+    .split(".")
+    .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
+    .join("");
 
   if (params) {
-    return cy[command](params)
-   }
-   else {
-    cy.log(command)
-     return cy?.[command]?.()
-   }
-})
+    return cy[command](params);
+  } else {
+    cy.log(command);
+    return cy?.[command]?.();
+  }
+});
