59d35e3fd175cb0b0452727eddba777f2f33ecd2
niliotiii <d.saiter@estudante.ifro.edu.br>
2024-05-23 19:59:36 -0400

    feat: add consulta and Delete Servidor

--- cypress/e2e/Telas/4 Cadastros/Servidores/servidor-cadastro.cy.js
+++ cypress/e2e/Telas/4 Cadastros/Servidores/servidor-cadastro.cy.js
@@ -1,13 +1,28 @@
-import { servidorFixtures as servidor } from "../../../../fixtures";
+import { servidorFixtures as servidor } from '../../../../fixtures';
 
-describe("INICIANDO TESTE", () => {
-  it("FAZENDO: Login", () => {
-    cy.commands("auth.Login", "ADMINISTRADOR");
+describe('INICIANDO TESTE', () => {
+  it('FAZENDO: Login', () => {
+    cy.commands('auth.Login', 'ADMINISTRADOR');
   });
-  it("ACESSANDO: Cadastrar Servidores", () => {
-    cy.visit("/dashboard/cadastro/servidor");
+  it('ACESSANDO: Cadastrar Servidores', () => {
+    cy.visit('/dashboard/cadastro/servidor');
   });
-  it("CADASTRANDO: Servidor", () => {
-    cy.commands("telas.cadastros.servidor", servidor);
+  it('CADASTRANDO: Servidor', () => {
+    cy.get(
+      'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
+    ).contains('Cadastrar Servidores');
+    cy.commands('telas.cadastros.servidor', servidor);
+  });
+  it('CONSULTANDO: Servidor', () => {
+    cy.get(
+      'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
+    ).contains('Consultar Servidores');
+    cy.commands('telas.consultas.servidor', servidor);
+  });
+  it('DELETANDO: Servidor', () => {
+    cy.get(
+      'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
+    ).contains('Consultar Servidores');
+    cy.commands('telas.consultas.servidor.delete', servidor);
   });
 });
