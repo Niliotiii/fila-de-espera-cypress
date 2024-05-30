5daf864b39893927ee981090abf364542f2b30c8
niliotiii <d.saiter@estudante.ifro.edu.br>
2024-05-29 20:14:27 -0400

    refactor: rename object

--- cypress/e2e/Telas/4 Cadastros/Criança/criança.cy.js
+++ cypress/e2e/Telas/4 Cadastros/Criança/criança.cy.js
@@ -1,4 +1,4 @@
-import { criancaFixtures } from '../../../../fixtures';
+import { criancaFixtures as crianca } from '../../../../fixtures';
 
 describe('INICIANDO TESTE', () => {
   it('FAZENDO: Login', () => {
@@ -13,63 +13,63 @@ describe('INICIANDO TESTE', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Cadastrar Criança');
-    cy.commands('telas.cadastros.crianca', criancaFixtures);
+    cy.commands('telas.cadastros.crianca', crianca);
   });
 
   it('CONSULTANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca', criancaFixtures);
+    cy.commands('telas.consultas.crianca', crianca);
   });
 
   it('VISUALIZANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca.visualizar', criancaFixtures);
+    cy.commands('telas.consultas.crianca.visualizar', crianca);
   });
 
   it('CONSULTANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca', criancaFixtures);
+    cy.commands('telas.consultas.crianca', crianca);
   });
 
   it('EDITANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca.editar', criancaFixtures);
+    cy.commands('telas.consultas.crianca.editar', crianca);
   });
-  // Apartir desse ponto criancaFixtures já foi alterado
+  // Apartir desse ponto crianca já foi alterado
 
   it('CONSULTANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca', criancaFixtures);
+    cy.commands('telas.consultas.crianca', crianca);
   });
 
   it('VISUALIZANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca.visualizar', criancaFixtures);
+    cy.commands('telas.consultas.crianca.visualizar', crianca);
   });
 
   it('CONSULTANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca', criancaFixtures);
+    cy.commands('telas.consultas.crianca', crianca);
   });
 
   it('DELETANDO: Criança', () => {
     cy.get(
       'body > div.relative.flex.min-h-screen.flex-col > div.flex-1 > div > div > main > div.flex.flex-row > div > div > span',
     ).contains('Consultar Crianças');
-    cy.commands('telas.consultas.crianca.deletar', criancaFixtures);
+    cy.commands('telas.consultas.crianca.deletar', crianca);
   });
 });
